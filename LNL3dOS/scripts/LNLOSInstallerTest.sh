07ab7de150b59ac896d1a0d34af85761626138e0
LNLMACHINE\albar <rcardosa@bcsgames.com>
2024-11-01 15:55:20 -0700

    syntax change

--- LNL3dOS/scripts/LNLOSInstallerTest.sh
+++ LNL3dOS/scripts/LNLOSInstallerTest.sh
@@ -9,7 +9,7 @@ update_lnl_os()
     report_status "Updating LNLOS"
     SOURCE_DIR="~/LNL3dOS"
     TARGER_DIR="~/printer_3_data"
-    sudo cp SOURCE_DIR TARGET_DIR
+    sudo cp "$SOURCE_DIR" "$TARGET_DIR"
 }
 
 
